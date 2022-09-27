17d316fa6a2487035f8c35cd73d0347d40b3861c
PetarDjukic1 <petardjukic23@gmail.com>
2022-09-27 16:18:04 +0200

    had to commit so I can pull from main

--- index.php
+++ index.php
@@ -10,9 +10,7 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
-    <style>
-  <?php include "style.css" ?>
-</style>
+    <link rel="stylesheet" href="style.css">
     <title>NASA</title>
 </head>
 <body>
